ca5c40c4548014ad4137b734ecb9418358acf289
Greg Jordan <gregljordan@gmail.com>
2019-07-16 17:26:02 -0400

    Built useLocalStorage function

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import useLocalStorage from './useLocalStorage'
+
+const useDarkMode = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default useDarkMode;
