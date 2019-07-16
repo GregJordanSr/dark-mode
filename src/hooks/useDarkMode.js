cdffa47b91219131406620201a2337c325fc53a0
Greg Jordan <gregljordan@gmail.com>
2019-07-16 18:25:45 -0400

    Imported DarkMode into Nav

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect  } from 'react';
 import useLocalStorage from './useLocalStorage'
 
-const useDarkMode = () => {
-   const [darkMan, setDarkMan] = useLocalStorage(key);
+const useDarkMode = (key, initialValue) => {
+   const [darkMan, setDarkMan] = useLocalStorage(key, initialValue);
    useEffect(() => {
-       darkMan ? document.body.classList.add(dark-man): document.body.classList.remove(dark-mode);
-   }, [input])
+       darkMan ? 
+        document.body.classList.add("dark-mode")
+        : 
+        document.body.classList.remove("dark-mode");
+   }
+   , [darkMan])
 
     return [darkMan, setDarkMan]
 }
